40b6091082e3f0e2f40dc41f3879b21e48763d14
martijnH96 <martijnh1996@hotmail.com>
2020-11-24 13:57:39 +0100

    initial commit

---
+++ src/main/java/LoginService.java
@@ -0,0 +1,9 @@
+public class LoginService {
+    public boolean login(String username, String password) {
+        if (username.equals("martijn") && password.equals("martijn")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}

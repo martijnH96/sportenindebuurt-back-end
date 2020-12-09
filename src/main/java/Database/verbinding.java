81a7b7b03a455215cabe056a504414f63325cdeb
ArraysZero <56938722+ArraysZero@users.noreply.github.com>
2020-12-09 18:33:58 +0100

    Update verbinding.java
    verbinding aangemaakt, voorbeeldcode staat hier: https://www.tutorialspoint.com/connecting-to-a-mysql-database-with-java

--- src/main/java/Database/verbinding.java
+++ src/main/java/Database/verbinding.java
@@ -1,4 +1,20 @@
 package Database;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public interface verbinding {
+	public default Connection maakVerbinding() throws SQLException { //dit moet worden afgevangen
+
+		//link naar de DB
+		String JDBCURL="jdbc:mysql://145.74.104.78:3306/sportenInDeBuurt";
+		Connection con = null;
+
+		//maak verbinding
+		con = DriverManager.getConnection(JDBCURL,"application","cM1l:Qze");
+		System.out.println("connection is established");
+
+		return con;
+	}
 }

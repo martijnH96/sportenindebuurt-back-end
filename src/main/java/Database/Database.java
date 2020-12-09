707348d9eb83a0b814caf6f32b10cc1ae9aa3e23
ArraysZero <56938722+ArraysZero@users.noreply.github.com>
2020-12-09 23:38:18 +0100

    Update Database.java
    aanmaken select query

--- src/main/java/Database/Database.java
+++ src/main/java/Database/Database.java
@@ -1,6 +1,8 @@
 package Database;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class Database implements verbinding{
@@ -15,19 +17,33 @@ public class Database implements verbinding{
 		}
 	}
 
-	public void select(String table, String columns, String where){
+	public ResultSet select(String table, String columns, String where) throws SQLException {
+		String sql = "SELECT " + columns + " FROM " + table;
+
+		PreparedStatement stmt = conn.prepareStatement(sql);
+
+		//stmt.setString(1, "*");
+		//stmt.setString(1, "Sport");
+
+		ResultSet results = stmt.executeQuery();
+
+		/* tijdens het testen in een andere app heb ik dit gebruikt
+		while (results.next()){
+			System.out.println(results.getString(1) + " ");
+		}*/
+
+		return results;
+	}
+
+	public void insert(String table, String columns, String data) throws SQLException {
 
 	}
 
-	public void insert(String table, String columns, String data){
+	public void update(String table, String columns, String data, String where) throws SQLException {
 
 	}
 
-	public void update(String table, String columns, String data, String where){
+	public void execute(String procedure, String[] values) throws SQLException {
 
-	}
-
-	public void execute(String procedure, String[] values){
-		
 	}
 }

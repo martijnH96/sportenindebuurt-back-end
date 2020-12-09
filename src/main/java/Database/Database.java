0351f622be8246406040aa30d25c4a35348b6e65
ArraysZero <56938722+ArraysZero@users.noreply.github.com>
2020-12-09 20:52:21 +0100

    Update Database.java
    alvast functies voor de queries klaar gezet, ga ze nu een voor een uitwerken en de juiste return waarde geven

--- src/main/java/Database/Database.java
+++ src/main/java/Database/Database.java
@@ -6,12 +6,28 @@ import java.sql.SQLException;
 public class Database implements verbinding{
 	private Connection conn;
 
-	private Database(){
+	public Database(){
 		try {
 			conn = maakVerbinding();
 		} catch (SQLException sql){
 			System.out.println("dit gaat fout");
 			sql.printStackTrace();
 		}
+	}
+
+	public void select(String table, String columns, String where){
+
+	}
+
+	public void insert(String table, String columns, String data){
+
+	}
+
+	public void update(String table, String columns, String data, String where){
+
+	}
+
+	public void execute(String procedure, String[] values){
+		
 	}
 }

22c426fca7d8d7dce864967d2ee964cfb6c20d74
ArraysZero <56938722+ArraysZero@users.noreply.github.com>
2020-12-09 19:19:38 +0100

    database constructor

--- src/main/java/Database/Database.java
+++ src/main/java/Database/Database.java
@@ -1,4 +1,17 @@
 package Database;
 
-public class Database {
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class Database implements verbinding{
+	private Connection conn;
+
+	private Database(){
+		try {
+			conn = maakVerbinding();
+		} catch (SQLException sql){
+			System.out.println("dit gaat fout");
+			sql.printStackTrace();
+		}
+	}
 }

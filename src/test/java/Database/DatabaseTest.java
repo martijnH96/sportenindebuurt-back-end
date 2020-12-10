fb62790c9234b0a18b5c27d509681b3a66c5b1a5
ArraysZero <56938722+ArraysZero@users.noreply.github.com>
2020-12-10 14:45:51 +0100

    Update DatabaseTest.java
    test setup aangemaakt

--- src/test/java/Database/DatabaseTest.java
+++ src/test/java/Database/DatabaseTest.java
@@ -6,4 +6,8 @@ import org.junit.rules.ExpectedException;
 public class DatabaseTest {
 	Database testDB;
 	public final ExpectedException exception = ExpectedException.none();
+
+	public void setup(){
+		testDB = new Database();
+	}
 }

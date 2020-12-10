b2709730a7e99c5ec25fe92a7ae7ab5931e75808
martijnH96 <62759944+martijnH96@users.noreply.github.com>
2020-12-10 09:34:13 +0100

    db properties added

--- src/main/java/LoginService.java
+++ src/main/java/LoginService.java
@@ -1,28 +1,30 @@
+import java.sql.*;
+
 public class LoginService {
 
-    String dbName = "DB4364163";
-    String dbUsername = "U4364163";
-    String dbPassword = "Icedmangowachtwoord123";
-    String serverName = "jdbc:mysql://rdbms.strato.de:3306/"+dbName+"?user="+dbUsername+"&password="+dbPassword;
+    String dbName = "sportenindebuurt";
+    String dbUsername = "root";
+    String dbPassword = "";
+    String serverName = "jdbc:mysql://localhost:3306/"+dbName;
 
     public boolean login(String username, String password) {
         //TODO Database verbinding
-//        try {
-//            Class.forName("com.mysql.cj.jdbc.Driver");
-//            Connection con = DriverManager.getConnection(serverName);
-//            PreparedStatement stmt = con.prepareStatement("SELECT * FROM 'user' WHERE 'Username' = ?");
-//            stmt.setString(1, "martijn");
-//            ResultSet result = stmt.executeQuery();
-//            if (result.first()){
-        if (username.equals("martijn") && password.equals("martijn")) {
-            return true;
-        } else {
-            return false;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection(serverName, dbUsername, dbPassword);
+            PreparedStatement stmt = con.prepareStatement("SELECT * FROM 'user' WHERE 'Username' = ?");
+            stmt.setString(1, username);
+            ResultSet result = stmt.executeQuery();
+            while (result.next()){
+                if (username.equals(result.getString("Username")) && password.equals(result.getString("Password"))) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        } catch (Exception e){
+            e.printStackTrace();
         }
-//            }
-//        } catch (Exception e){
-//            e.printStackTrace();
-//        }
-//        return false;
+        return false;
     }
 }

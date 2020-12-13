bc6ec9a06a8caacdbf28bb6b5c66508e5464e19b
ArraysZero <56938722+ArraysZero@users.noreply.github.com>
2020-12-13 17:41:54 +0100

    Update VerbindingTest.java
    alles overgecopieerd dat ik al had

--- src/test/java/Database/VerbindingTest.java
+++ src/test/java/Database/VerbindingTest.java
@@ -1,4 +1,42 @@
 package Database;
 
+//import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+//import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.rules.ExpectedException;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class VerbindingTest {
+	// ----------------- //
+	// verbinding testen //
+	// ----------------- //
+
+	@Test
+	void maakVerbindingTestCorrect(){
+		//test de verbinding juist
+	}
+
+	@Test
+	void maakVerbindingTestWrongAddres(){
+		//test verbinding met verkeerde adres link
+	}
+
+	@Test
+	void maakVerbindingTestWrongDB(){
+		//test verbinding met verkeerde DB
+	}
+
+	@Test
+	void maakVerbindingTestWrongUser(){
+		//test verbinding verkeerde user
+	}
+
+	@Test
+	void maakVerbindingTestWrongPassword(){
+		//test verbinding met verkeerde wachtwoord
+	}
 }

6bbddf07c75b9b0f089fbc3f39409ece057bbb5f
ArraysZero <56938722+ArraysZero@users.noreply.github.com>
2020-12-15 17:09:23 +0100

    Update SportDTO.java
    Sport DTO is klaar

--- src/main/java/DTO/SportDTO.java
+++ src/main/java/DTO/SportDTO.java
@@ -1,4 +1,47 @@
 package DTO;
 
 public class SportDTO {
+	private String name;
+	private boolean isTeamSport;
+	private int maxPlayers;
+	private int minPlayers;
+
+	public SportDTO(String name, boolean isTeamSport, int maxPlayers, int minPlayers) {
+		this.name = name;
+		this.isTeamSport = isTeamSport;
+		this.maxPlayers = maxPlayers;
+		this.minPlayers = minPlayers;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public boolean isTeamSport() {
+		return isTeamSport;
+	}
+
+	public int getMaxPlayers() {
+		return maxPlayers;
+	}
+
+	public int getMinPlayers() {
+		return minPlayers;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setTeamSport(boolean teamSport) {
+		isTeamSport = teamSport;
+	}
+
+	public void setMaxPlayers(int maxPlayers) {
+		this.maxPlayers = maxPlayers;
+	}
+
+	public void setMinPlayers(int minPlayers) {
+		this.minPlayers = minPlayers;
+	}
 }

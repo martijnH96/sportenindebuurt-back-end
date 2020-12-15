69d051a05bd358fe092ac1c44fc766e942d1088b
ArraysZero <56938722+ArraysZero@users.noreply.github.com>
2020-12-15 14:45:23 +0100

    Update UserDTO.java
    belangerijke info user

--- src/main/java/DTO/UserDTO.java
+++ src/main/java/DTO/UserDTO.java
@@ -1,18 +1,37 @@
 package DTO;
 
 public class UserDTO {
-    private String user;
-    private String password;
-    public String getUser() {
-        return user;
-    }
-    public String getPassword() {
-        return password;
-    }
-    public void setUser(String user) {
-        this.user = user;
-    }
-    public void setPassword(String password) {
-        this.password = password;
-    }
+	private String name;
+	private String email;
+	private String adres;
+
+	public UserDTO(String name, String email, String adres) {
+		this.name = name;
+		this.email = email;
+		this.adres = adres;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getAdres() {
+		return adres;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public void setAdres(String adres) {
+		this.adres = adres;
+	}
 }

cb86f6f60f5aa8e5eca6e65d0867d961a57a7e8c
ArraysZero <56938722+ArraysZero@users.noreply.github.com>
2020-12-15 17:07:21 +0100

    Update EventDTO.java
    Event DTO klaar

--- src/main/java/DTO/EventDTO.java
+++ src/main/java/DTO/EventDTO.java
@@ -1,4 +1,70 @@
 package DTO;
 
+import java.util.ArrayList;
+
 public class EventDTO {
+	private String sport;
+	private int field;
+	private int numberOfParticipants;
+	private ArrayList<String> participants;
+	private String postalcode;
+	private int housenumber;
+
+	public EventDTO(String sport, int field, int numberOfParticipants,
+					ArrayList<String> participants, String postalcode, int housenumber) {
+		this.sport = sport;
+		this.field = field;
+		this.numberOfParticipants = numberOfParticipants;
+		this.participants = participants;
+		this.postalcode = postalcode;
+		this.housenumber = housenumber;
+	}
+
+	public String getSport() {
+		return sport;
+	}
+
+	public int getField() {
+		return field;
+	}
+
+	public int getNumberOfParticipants() {
+		return numberOfParticipants;
+	}
+
+	public ArrayList<String> getParticipants() {
+		return participants;
+	}
+
+	public String getPostalcode() {
+		return postalcode;
+	}
+
+	public int getHousenumber() {
+		return housenumber;
+	}
+
+	public void setSport(String sport) {
+		this.sport = sport;
+	}
+
+	public void setField(int field) {
+		this.field = field;
+	}
+
+	public void setNumberOfParticipants(int numberOfParticipants) {
+		this.numberOfParticipants = numberOfParticipants;
+	}
+
+	public void setParticipants(ArrayList<String> participants) {
+		this.participants = participants;
+	}
+
+	public void setPostalcode(String postalcode) {
+		this.postalcode = postalcode;
+	}
+
+	public void setHousenumber(int housenumber) {
+		this.housenumber = housenumber;
+	}
 }

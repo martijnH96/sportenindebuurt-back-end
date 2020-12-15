b18194e99acd82e46d5caf52c639ed0b5de98777
ArraysZero <56938722+ArraysZero@users.noreply.github.com>
2020-12-15 16:49:32 +0100

    Update UserDTO.java
    alles juist kwa data

--- src/main/java/DTO/UserDTO.java
+++ src/main/java/DTO/UserDTO.java
@@ -1,37 +1,81 @@
 package DTO;
 
-public class UserDTO {
-	private String name;
-	private String email;
-	private String adres;
+import java.sql.Date;
+import java.text.SimpleDateFormat;
 
-	public UserDTO(String name, String email, String adres) {
+public class UserDTO {
+	private String name; //sporter
+	private String lastname; //sporter
+	private String mail; //sporter
+	private SimpleDateFormat dateOfBirth; //sporter
+	private String postalcode; //locatie
+	private int houseNumber; //locatie
+	private String streetName; //locatie
+
+	public UserDTO(String name, String lastname, String mail, SimpleDateFormat dateOfBirth,
+				   String postalcode, int houseNumber, String streetName) {
 		this.name = name;
-		this.email = email;
-		this.adres = adres;
+		this.lastname = lastname;
+		this.mail = mail;
+		this.dateOfBirth = dateOfBirth;
+		this.postalcode = postalcode;
+		this.houseNumber = houseNumber;
+		this.streetName = streetName;
 	}
 
 	public String getName() {
 		return name;
 	}
 
-	public String getEmail() {
-		return email;
+	public String getLastname() {
+		return lastname;
 	}
 
-	public String getAdres() {
-		return adres;
+	public String getMail() {
+		return mail;
+	}
+
+	public SimpleDateFormat getDateOfBirth() {
+		return dateOfBirth;
+	}
+
+	public String getPostalcode() {
+		return postalcode;
+	}
+
+	public int getHouseNumber() {
+		return houseNumber;
+	}
+
+	public String getStreetName() {
+		return streetName;
 	}
 
 	public void setName(String name) {
 		this.name = name;
 	}
 
-	public void setEmail(String email) {
-		this.email = email;
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
 	}
 
-	public void setAdres(String adres) {
-		this.adres = adres;
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+
+	public void setDateOfBirth(SimpleDateFormat dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+
+	public void setPostalcode(String postalcode) {
+		this.postalcode = postalcode;
+	}
+
+	public void setHouseNumber(int houseNumber) {
+		this.houseNumber = houseNumber;
+	}
+
+	public void setStreetName(String streetName) {
+		this.streetName = streetName;
 	}
 }

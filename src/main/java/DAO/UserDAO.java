13f4fe4661d48d2975e958301fa11c0056dfe17c
ArraysZero <56938722+ArraysZero@users.noreply.github.com>
2020-12-16 14:55:24 +0100

    Update UserDAO.java

--- src/main/java/DAO/UserDAO.java
+++ src/main/java/DAO/UserDAO.java
@@ -1,4 +1,22 @@
 package DAO;
 
+import DTO.UserDTO;
+
 public interface UserDAO {
+	//select all users
+	public UserDTO[] selectAll();
+
+	//select singular user (id)
+	public UserDTO selectId(int id);
+
+	//select user using where
+	public UserDTO[] selectWhere(String[] statements, String[] variables);
+
+	//update user
+	public void updateUser(String[] tables, String[] newValues, int id);
+
+	//update user addres
+	public void updateAdres(String[])
+
+	//insert new user
 }

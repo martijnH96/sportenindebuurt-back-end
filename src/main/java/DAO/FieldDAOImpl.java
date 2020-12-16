dad7a1e3befe58165ba3f709f46ad3346df75da4
ArraysZero <56938722+ArraysZero@users.noreply.github.com>
2020-12-16 16:41:32 +0100

    Update FieldDAOImpl.java

--- src/main/java/DAO/FieldDAOImpl.java
+++ src/main/java/DAO/FieldDAOImpl.java
@@ -1,4 +1,41 @@
 package DAO;
 
-public class FieldDAOImpl {
+import DTO.FieldDTO;
+import Database.Database;
+
+import java.util.ArrayList;
+
+public class FieldDAOImpl implements FieldDAO{
+	private ArrayList<FieldDTO> Fields;
+	private Database data;
+
+	public FieldDAOImpl(ArrayList<FieldDTO> fields) {
+		Fields = fields;
+		data = new Database();
+	}
+
+	@Override
+	public FieldDTO[] selectAll() {
+		return new FieldDTO[0];
+	}
+
+	@Override
+	public FieldDTO selectId(int id) {
+		return null;
+	}
+
+	@Override
+	public FieldDTO[] selectWhere(String[] statements, String[] values) {
+		return new FieldDTO[0];
+	}
+
+	@Override
+	public void update(String[] tables, String[] values) {
+
+	}
+
+	@Override
+	public void insert() {
+
+	}
 }

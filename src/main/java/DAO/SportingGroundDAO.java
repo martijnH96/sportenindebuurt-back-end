6bfc5af5b350dafb71c3cd2da8c655fc0d5871e8
ArraysZero <56938722+ArraysZero@users.noreply.github.com>
2020-12-16 16:20:26 +0100

    Update SportingGroundDAO.java

--- src/main/java/DAO/SportingGroundDAO.java
+++ src/main/java/DAO/SportingGroundDAO.java
@@ -1,13 +1,22 @@
 package DAO;
 
+import DTO.SportingGroundDTO;
+
 public interface SportingGroundDAO {
 	//select all
+	public SportingGroundDTO[] selectAll();
 
 	//select specific
+	public SportingGroundDTO selectID(int id);
 
 	//select on category
+	public SportingGroundDTO[] selectWhere(String where);
 
 	//update
+	public void update(String[] tables, String[] values);
 
 	//insert new
+	public void newSportingGround();
+	//dit moet nog worden aangepast maar kan nu niets ccontroleren
+	//omdat t-mobile er weer uitligt
 }

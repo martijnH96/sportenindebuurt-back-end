cb41e39410c825342d1085b015017ca18c21b4bb
ArraysZero <56938722+ArraysZero@users.noreply.github.com>
2020-12-16 10:38:11 +0100

    Update FieldDTO.java
    alle DTO's zijn klaar

--- src/main/java/DTO/FieldDTO.java
+++ src/main/java/DTO/FieldDTO.java
@@ -1,4 +1,27 @@
 package DTO;
 
 public class FieldDTO {
+	private String sport;
+	private int Capacity;
+
+	public FieldDTO(String sport, int capacity) {
+		this.sport = sport;
+		Capacity = capacity;
+	}
+
+	public String getSport() {
+		return sport;
+	}
+
+	public int getCapacity() {
+		return Capacity;
+	}
+
+	public void setSport(String sport) {
+		this.sport = sport;
+	}
+
+	public void setCapacity(int capacity) {
+		Capacity = capacity;
+	}
 }

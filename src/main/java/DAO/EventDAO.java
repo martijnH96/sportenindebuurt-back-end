c1f0153952b3d60558e78d46a804a7f1d297d3aa
ArraysZero <56938722+ArraysZero@users.noreply.github.com>
2020-12-16 16:44:49 +0100

    Update EventDAO.java

--- src/main/java/DAO/EventDAO.java
+++ src/main/java/DAO/EventDAO.java
@@ -1,4 +1,20 @@
 package DAO;
 
+import DTO.EventDTO;
+
 public interface EventDAO {
+	//select all
+	public EventDTO[] selectAll();
+
+	//select specific
+	public EventDTO selectId(int id);
+
+	//select on category
+	public EventDTO[] selectWhere(String[] statements, String[] values);
+
+	//update
+	public void update(String[] tables, String[] values);
+
+	//insert
+	public void insert(); //kut t-mobile
 }

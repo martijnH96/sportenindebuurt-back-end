bab8045ee5787d2f43dfae79604d7be592a550d2
ArraysZero <56938722+ArraysZero@users.noreply.github.com>
2020-12-16 16:33:50 +0100

    Update SportDAOImpl.java

--- src/main/java/DAO/SportDAOImpl.java
+++ src/main/java/DAO/SportDAOImpl.java
@@ -5,7 +5,37 @@ import Database.Database;
 
 import java.util.ArrayList;
 
-public class SportDAOImpl {
+public class SportDAOImpl implements SportDAO{
 	private ArrayList<SportDTO> sports;
 	private Database data;
+
+	public SportDAOImpl(ArrayList<SportDTO> sports) {
+		this.sports = sports;
+		this.data = new Database();
+	}
+
+	@Override
+	public SportDTO[] selectAll() {
+		return new SportDTO[0];
+	}
+
+	@Override
+	public SportDTO selectId(int id) {
+		return null;
+	}
+
+	@Override
+	public SportDTO[] selectWhere(String[] statements, String[] variables) {
+		return new SportDTO[0];
+	}
+
+	@Override
+	public void newSport() {
+
+	}
+
+	@Override
+	public void update(String[] tables, String[] values) {
+
+	}
 }

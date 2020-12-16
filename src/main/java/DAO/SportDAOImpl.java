1c4ec2519fdf791b1c482427abdf2f4216d0c327
ArraysZero <56938722+ArraysZero@users.noreply.github.com>
2020-12-16 16:23:30 +0100

    DAO implements

--- src/main/java/DAO/SportDAOImpl.java
+++ src/main/java/DAO/SportDAOImpl.java
@@ -1,4 +1,11 @@
 package DAO;
 
+import DTO.SportDTO;
+import Database.Database;
+
+import java.util.ArrayList;
+
 public class SportDAOImpl {
+	private ArrayList<SportDTO> sports;
+	private Database data;
 }
--- src/main/java/DAO/SportingGroundDAOImpl.java
+++ src/main/java/DAO/SportingGroundDAOImpl.java
@@ -1,4 +1,36 @@
 package DAO;
 
-public class SportingGroundDAOImpl {
+import DTO.SportingGroundDTO;
+import Database.Database;
+
+import java.util.ArrayList;
+
+public class SportingGroundDAOImpl implements SportingGroundDAO{
+	private ArrayList<SportingGroundDTO> sportingGounds;
+	private Database data;
+
+	@Override
+	public SportingGroundDTO[] selectAll() {
+		return new SportingGroundDTO[0];
+	}
+
+	@Override
+	public SportingGroundDTO selectID(int id) {
+		return null;
+	}
+
+	@Override
+	public SportingGroundDTO[] selectWhere(String where) {
+		return new SportingGroundDTO[0];
+	}
+
+	@Override
+	public void update(String[] tables, String[] values) {
+
+	}
+
+	@Override
+	public void newSportingGround() {
+
+	}
 }

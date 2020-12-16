4fe41d089e44fe5e1e9ea10dcd0c677251f8cf80
ArraysZero <56938722+ArraysZero@users.noreply.github.com>
2020-12-16 16:46:05 +0100

    Update EventDAOImpl.java

--- src/main/java/DAO/EventDAOImpl.java
+++ src/main/java/DAO/EventDAOImpl.java
@@ -1,4 +1,41 @@
 package DAO;
 
-public class EventDAOImpl {
+import DTO.EventDTO;
+import Database.Database;
+
+import java.util.ArrayList;
+
+public class EventDAOImpl implements EventDAO{
+	private ArrayList<EventDTO> events;
+	private Database data;
+
+	public EventDAOImpl(ArrayList<EventDTO> events) {
+		this.events = events;
+		data = new Database();
+	}
+
+	@Override
+	public EventDTO[] selectAll() {
+		return new EventDTO[0];
+	}
+
+	@Override
+	public EventDTO selectId(int id) {
+		return null;
+	}
+
+	@Override
+	public EventDTO[] selectWhere(String[] statements, String[] values) {
+		return new EventDTO[0];
+	}
+
+	@Override
+	public void update(String[] tables, String[] values) {
+
+	}
+
+	@Override
+	public void insert() {
+
+	}
 }

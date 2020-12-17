79795127f77859eb53fd38c725d14742c1c01bd7
ArraysZero <56938722+ArraysZero@users.noreply.github.com>
2020-12-17 15:43:48 +0100

    Update LocationDAOImpl.java

--- src/main/java/DAO/LocationDAOImpl.java
+++ src/main/java/DAO/LocationDAOImpl.java
@@ -1,4 +1,49 @@
 package DAO;
 
-public class LocationDAOImpl {
+import DTO.LocationDTO;
+import Database.Database;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class LocationDAOImpl implements LocationDAO{
+	Database data = new Database();
+
+	public LocationDAOImpl() {
+	}
+
+	@Override
+	public LocationDTO[] selectAll() throws SQLException {
+		return new LocationDTO[0];
+	}
+
+	@Override
+	public LocationDTO selectId(int id) throws SQLException {
+		return null;
+	}
+
+	@Override
+	public LocationDTO[] selectWhere(String[] statements, String[] variables) throws SQLException {
+		return new LocationDTO[0];
+	}
+
+	@Override
+	public void update(LocationDTO location) throws SQLException {
+
+	}
+
+	@Override
+	public void insert(LocationDTO location) throws SQLException {
+		Connection conn = data.verbind();
+
+		String table = "Sporter";
+		String columns = "";
+		String[] dataValues = {user.getName(), user.getDateOfBirth() + "", user.getLastname(), user.getEmail(),
+				user.getPassword(), user.getAdres() + ""};
+		String[] where = {"id = ", "" + user.getID()};
+
+		data.insert(table, columns, dataValues, conn);
+
+		data.closeConnection(conn);
+	}
 }

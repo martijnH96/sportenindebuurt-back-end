9a0438de73aea4e92e7aec03962559fd52ae1855
ArraysZero <56938722+ArraysZero@users.noreply.github.com>
2020-12-17 15:38:49 +0100

    Update LocationDTO.java

--- src/main/java/DTO/LocationDTO.java
+++ src/main/java/DTO/LocationDTO.java
@@ -1,4 +1,47 @@
 package DTO;
 
 public class LocationDTO {
+	private int id;
+	private String postalcode;
+	private int housenumber;
+	private int streetname;
+
+	public LocationDTO(int id, String postalcode, int housenumber, int streetname) {
+		this.id = id;
+		this.postalcode = postalcode;
+		this.housenumber = housenumber;
+		this.streetname = streetname;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public String getPostalcode() {
+		return postalcode;
+	}
+
+	public int getHousenumber() {
+		return housenumber;
+	}
+
+	public int getStreetname() {
+		return streetname;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public void setPostalcode(String postalcode) {
+		this.postalcode = postalcode;
+	}
+
+	public void setHousenumber(int housenumber) {
+		this.housenumber = housenumber;
+	}
+
+	public void setStreetname(int streetname) {
+		this.streetname = streetname;
+	}
 }

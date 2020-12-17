e778080f3148bbaf863c69565f316ed352965e79
ArraysZero <56938722+ArraysZero@users.noreply.github.com>
2020-12-17 15:41:49 +0100

    Update LocationDAO.java

--- src/main/java/DAO/LocationDAO.java
+++ src/main/java/DAO/LocationDAO.java
@@ -1,4 +1,17 @@
 package DAO;
 
+import DTO.LocationDTO;
+
+import java.sql.SQLException;
+
 public interface LocationDAO {
+	public LocationDTO[] selectAll() throws SQLException;
+
+	public LocationDTO selectId(int id) throws SQLException;
+
+	public LocationDTO[] selectWhere(String[] statements, String[] variables) throws SQLException;
+
+	public void update(LocationDTO location) throws SQLException;
+
+	public void insert(LocationDTO location) throws SQLException;
 }

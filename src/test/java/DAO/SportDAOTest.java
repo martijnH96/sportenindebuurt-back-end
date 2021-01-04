f55d9835c3661d9b6b883448256e20114953f196
ArraysZero <56938722+ArraysZero@users.noreply.github.com>
2021-01-04 13:32:26 +0100

    selects doen het

--- src/test/java/DAO/SportDAOTest.java
+++ src/test/java/DAO/SportDAOTest.java
@@ -66,7 +66,7 @@ public class SportDAOTest {
 		//arrange
 		SportDTO[] sports = null;
 		String[] wheres = {"naam = "};
-		String[] name = {"0"};
+		String[] name = {"bestaat niet"};
 
 		//act
 		try {

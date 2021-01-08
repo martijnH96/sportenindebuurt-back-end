8d4fa9c66f6fb6f4324d10d2de765439dcda5836
martijnH96 <martijnh1996@hotmail.com>
2021-01-08 13:38:33 +0100

    ready for test

--- src/main/java/LoginService.java
+++ src/main/java/LoginService.java
@@ -2,27 +2,21 @@ import java.sql.*;
 
 public class LoginService {
 
-    String dbName = "sportenindebuurt";
-    String dbUsername = "root";
-    String dbPassword = "";
-    String serverName = "jdbc:mysql://localhost:3306/"+dbName;
+    final String dbUsername = "application";
+    final String dbPassword = "cM1l:Qze";
+    final String serverName = "jdbc:mysql://145.74.104.78:3306/sportenInDeBuurt";
 
     public boolean login(String username, String password) {
-        //TODO Database verbinding
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             Connection con = DriverManager.getConnection(serverName, dbUsername, dbPassword);
-            PreparedStatement stmt = con.prepareStatement("SELECT * FROM 'user' WHERE 'Username' = ?");
+            PreparedStatement stmt = con.prepareStatement("SELECT * FROM Sporter WHERE email = ?");
             stmt.setString(1, username);
             ResultSet result = stmt.executeQuery();
-            while (result.next()){
-                if (username.equals(result.getString("Username")) && password.equals(result.getString("Password"))) {
-                    return true;
-                } else {
-                    return false;
-                }
+            while (result.next()) {
+                return username.equals(result.getString("email")) && password.equals(result.getString("wachtwoord"));
             }
-        } catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return false;

d5ffc641dfce50c44d41096f8ccadb1209185683
ArraysZero <56938722+ArraysZero@users.noreply.github.com>
2021-01-09 13:04:45 +0100

    Create SportingGroundDAOTest.java

---
+++ src/test/java/persistence/SportingGroundDAOTest.java
@@ -0,0 +1,20 @@
+package persistence;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+public class SportingGroundDAOTest {
+	private SportingGroundDAOImpl sut; //system under test
+
+	@BeforeEach
+	public void before(){
+		//maybe one day
+	}
+
+	@AfterEach
+	public void after(){
+		//maybe one day
+	}
+
+	//hier komen de tests
+}

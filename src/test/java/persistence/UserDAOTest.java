dd0857cfa577ffcd7330bc8d227fc010d113f195
ArraysZero <56938722+ArraysZero@users.noreply.github.com>
2021-01-09 13:02:24 +0100

    Create UserDAOTest.java

---
+++ src/test/java/persistence/UserDAOTest.java
@@ -0,0 +1,20 @@
+package persistence;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+public class UserDAOTest {
+	private UserDAOImpl sut; //system under test
+
+	@BeforeEach
+	public void before(){
+		//mss ooit
+	}
+
+	@AfterEach
+	public void after(){
+		//mss ooit
+	}
+
+	//hier komen de tests
+}

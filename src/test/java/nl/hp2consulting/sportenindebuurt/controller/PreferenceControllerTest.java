661d82ec3e1b31ddc1ef0682e1fbac5567df3163
martijnH96 <martijnh1996@hotmail.com>
2021-01-12 12:18:31 +0100

    package namen update, dao package toegevoegd (moet nog code verplaatsen), test map aangemaakt

---
+++ src/test/java/nl/hp2consulting/sportenindebuurt/controller/PreferenceControllerTest.java
@@ -0,0 +1,30 @@
+package nl.hp2consulting.sportenindebuurt.controller;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class PreferenceControllerTest {
+
+
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getSports() {
+    }
+
+    @Test
+    void insertPreferenceTime() {
+    }
+
+    @Test
+    void insertPreferenceSports() {
+    }
+}
---
+++ src/test/java/nl/hp2consulting/sportenindebuurt/controller/SportControllerTest.java
@@ -0,0 +1,19 @@
+package nl.hp2consulting.sportenindebuurt.controller;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class SportControllerTest {
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getSports() {
+    }
+}

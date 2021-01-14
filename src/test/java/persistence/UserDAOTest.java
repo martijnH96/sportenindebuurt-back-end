a9957c3a93df2f025eded0c85fbac915ad0bf1fa
ArraysZero <56938722+ArraysZero@users.noreply.github.com>
2021-01-14 17:32:25 +0100

    eerste test werkt

--- src/test/java/persistence/UserDAOTest.java
+++ src/test/java/persistence/UserDAOTest.java
@@ -1,8 +1,13 @@
 package persistence;
 
+import org.h2.tools.RunScript;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.io.InputStreamReader;
+import java.sql.SQLException;
 
 public class UserDAOTest {
 	private UserDAOImpl sut; //system under test
@@ -18,9 +23,16 @@ public class UserDAOTest {
 		//mss ooit
 	}
 
+	private void loadDatabaseFixture(String filename) throws SQLException, sportenInDeBuurtPersistenceException {
+		RunScript.execute(new ConnectionFactory().getConnection(),
+				new InputStreamReader(this.getClass().getResourceAsStream("/fixtures/" + filename)));
+	}
+
 	//hier komen de tests
 	@Test
-	public void selectAllTest() throws sportenInDeBuurtPersistenceException {
-		var actualAccounts = sut.selectAll();
+	public void selectAllTest() throws Exception {
+		loadDatabaseFixture("UserBaseData.sql");
+		var actualUsers = sut.selectAll();
+		Assertions.assertEquals(2, actualUsers.size());
 	}
 }
--- src/test/resources/fixtures/UserBaseData.sql
+++ src/test/resources/fixtures/UserBaseData.sql
@@ -1,11 +1,28 @@
 DROP TABLE IF EXISTS Sporter;
 DROP TABLE IF EXISTS Locatie;
 
-CREATE TABLE Locatie(adresNr INT, postcode VARCHAR(6), huisnummer INT, straatnaam VARCHAR (100));
-CREATE TABLE Sporter(SporterId INT, naam VARCHAR(100), DateOfBirth)
+CREATE TABLE Locatie(
+    adresNr INT NOT NULL AUTO_INCREMENT,
+    postcode VARCHAR(6),
+    huisnummer INT,
+    straatnaam VARCHAR (100),
+    PRIMARY KEY (adresNr)
+    );
 
-INSERT INTO track values (1,'abc');
-INSERT INTO track values (2,'bcd');
+CREATE TABLE Sporter(
+    SporterId INT NOT NULL AUTO_INCREMENT,
+    naam VARCHAR(100),
+    DateOfBirth DATE,
+    achternaam VARCHAR(100),
+    email VARCHAR(100),
+    wachtwoord VARCHAR(255),
+    Locatie_adresNr INT,
+    PRIMARY KEY (SporterId),
+    FOREIGN KEY (Locatie_adresNr) REFERENCES Locatie(adresNr)
+    );
 
-INSERT INTO accounts values ('one','onepass');
-INSERT INTO accounts values ('two','twopass');
+INSERT INTO Locatie VALUES (1, '6828XX', 17, 'Spoorwegstraat');
+INSERT INTO Locatie VALUES (2, '6942OS', 69, 'testerseweg');
+
+INSERT INTO Sporter values (1, 'martijn', '1996-12-28', 'hogendoorn', 'martijn@gmail.com', 'mrtijn', 1);
+INSERT INTO Sporter values (2, 'tester', '1970-01-01', 'test', 'testertest@gmail.com', 'test', 1);

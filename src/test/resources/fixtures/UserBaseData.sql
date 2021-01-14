659996d2a0a421da40e0bf859f0f246626efa71b
ArraysZero <56938722+ArraysZero@users.noreply.github.com>
2021-01-14 16:33:51 +0100

    aanmaken test DB

---
+++ src/test/resources/fixtures/UserBaseData.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS Sporter;
+DROP TABLE IF EXISTS Locatie;
+
+CREATE TABLE Locatie(adresNr INT, postcode VARCHAR(6), huisnummer INT, straatnaam VARCHAR (100));
+CREATE TABLE Sporter(SporterId INT, naam VARCHAR(100), DateOfBirth)
+
+INSERT INTO track values (1,'abc');
+INSERT INTO track values (2,'bcd');
+
+INSERT INTO accounts values ('one','onepass');
+INSERT INTO accounts values ('two','twopass');
